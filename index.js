234ea2f3168e86a79b2b99b84e58696b9450269f
mrpratik01 <bhattaraipratik663@gmail.com>
2021-05-23 21:15:43 +0545

    commit

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log('hey there');
+alert('hello this is pratik bhattarai ! welcome to my site')
+
+var word = prompt('what is your role in college');
+document.getElementById('text') .innerHTML = word;
