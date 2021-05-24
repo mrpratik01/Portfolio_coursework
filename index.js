6240621dc4165074704e9d4e93062420585c6a42
mrpratik01 <bhattaraipratik663@gmail.com>
2021-05-24 18:41:31 +0545

    some skill bar changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-console.log('hey there');
-alert('hello this is pratik bhattarai ! welcome to my site')
+// console.log('hey there');
+// alert('hello this is pratik bhattarai ! welcome to my site')
 
-var word = prompt('what is your role in college');
-document.getElementById('text') .innerHTML = word;
+// var word = prompt('what is your role in college');
+// document.getElementById('text') .innerHTML = word;
